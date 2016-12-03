2b254d93ec5d2807dffb3a34ac7c9a6cd84523bc
Ramiro Romero Dapozo <ramiroromero94@gmail.com>
2016-12-03 00:42:06 -0300

    Chequeo de superusuario.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
-#include "gui/PartitionManagerWindow.h"
+#include <unistd.h>
+#include <sys/types.h>
+
 #include <QApplication>
 #include <QTranslator>
+#include <QMessageBox>
+
+
+#include "gui/PartitionManagerWindow.h"
 
 int main(int argc, char *argv[])
 {
@@ -14,8 +20,18 @@ int main(int argc, char *argv[])
 
     a.setApplicationName(QObject::tr("Partition Manager"));
     a.setApplicationDisplayName(QObject::tr("Partition Manager"));
-    PartitionManagerWindow w;
-    w.show();
 
-    return a.exec();
+    if(getuid()==0)
+    {
+        PartitionManagerWindow w;
+        w.show();
+        return a.exec();
+    }else{
+        QMessageBox::warning(nullptr,
+                             QObject::tr("Superuser privileges required."),
+                             QObject::tr("This program needs superuser privileges to access storage devices. Please run this program again as root."));
+        return -1;
+    }
+
+
 }
