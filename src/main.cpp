1acc8ed8f26ad21c298790229702259ca33753e3
Ramiro Romero Dapozo <ramiroromero94@gmail.com>
2016-12-08 22:46:14 -0300

    Verificar unicidad del proceso

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,21 @@
+#include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/file.h>
 
 #include <QApplication>
 #include <QTranslator>
 #include <QMessageBox>
 
-
 #include "gui/PartitionManagerWindow.h"
+
+
+bool isProcessUnique()
+{
+    int instanceFile = open("/tmp/pmgr.pid", O_CREAT | O_RDWR, 0666);
+    int lock = flock(instanceFile, LOCK_EX | LOCK_NB);
+    return !lock;
+}
 
 int main(int argc, char *argv[])
 {
@@ -16,22 +25,26 @@ int main(int argc, char *argv[])
     translator.load("partmgr_es");
     a.installTranslator(&translator);
 
-
-
     a.setApplicationName(QObject::tr("Partition Manager"));
     a.setApplicationDisplayName(QObject::tr("Partition Manager"));
 
-    if(getuid()==0)
+    if(!isProcessUnique())
     {
-        PartitionManagerWindow w;
-        w.show();
-        return a.exec();
-    }else{
+        QMessageBox::warning(nullptr,
+                             QObject::tr("Another instance is open"),
+                             QObject::tr("There is another instance of this program running. Running multiple instances is not supported."));
+        return -1;
+    }
+
+    if(getuid()!=0)
+    {
         QMessageBox::warning(nullptr,
                              QObject::tr("Superuser privileges required."),
                              QObject::tr("This program needs superuser privileges to access storage devices. Please run this program again as root."));
         return -1;
     }
 
-
+    PartitionManagerWindow w;
+    w.show();
+    return a.exec();
 }
