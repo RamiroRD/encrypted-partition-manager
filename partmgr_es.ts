2292c9b9185df611c5f2f81e952fb48dbd56e745
Ramiro Romero Dapozo <ramiroromero94@gmail.com>
2016-12-15 01:35:14 -0300

    Traducciones para lo hecho último.

--- partmgr_es.ts
+++ partmgr_es.ts
@@ -4,13 +4,27 @@
 <context>
     <name>CreateDialog</name>
     <message>
-        <location filename="src/gui/CreateDialog.cpp" line="26"/>
         <source>Offset:</source>
         <translatorcomment>(Compensación)</translatorcomment>
-        <translation>Offset</translation>
+        <translation type="vanished">Offset</translation>
     </message>
     <message>
-        <location filename="src/gui/CreateDialog.cpp" line="27"/>
+        <location filename="src/gui/CreateDialog.cpp" line="15"/>
+        <source>Choose a password and position number. A position number is in the range 0-</source>
+        <translation>Elija una contraseña y un número de posición. Un número de posición está en el rango 0-</translation>
+    </message>
+    <message>
+        <location filename="src/gui/CreateDialog.cpp" line="17"/>
+        <source>. The size of the gap between two consecutives positions is proportional to the device size.</source>
+        <translation>. El tamaño del hueco entre dos posiciones consecutivas es proporcional al tamaño del dispositivo.</translation>
+    </message>
+    <message>
+        <location filename="src/gui/CreateDialog.cpp" line="38"/>
+        <source>Position:</source>
+        <translation>Posición:</translation>
+    </message>
+    <message>
+        <location filename="src/gui/CreateDialog.cpp" line="39"/>
         <source>Password:</source>
         <translation>Contraseña:</translation>
     </message>
@@ -23,7 +37,7 @@
         <location filename="src/gui/PartitionManagerAdapter.cpp" line="141"/>
         <source>System call: &quot;</source>
         <translatorcomment>System call:</translatorcomment>
-        <translation>Llamada de sistema:</translation>
+        <translation>Llamada de sistema: &quot;</translation>
     </message>
     <message>
         <location filename="src/gui/PartitionManagerAdapter.cpp" line="41"/>
@@ -35,7 +49,7 @@
         <location filename="src/gui/PartitionManagerAdapter.cpp" line="66"/>
         <source>Device wipe failed.</source>
         <translatorcomment>Falló la limpieza del dispositivo</translatorcomment>
-        <translation>Surgió un error en la limpieza del dispositivo</translation>
+        <translation>Falló la limpieza del dispositivo.</translation>
     </message>
     <message>
         <location filename="src/gui/PartitionManagerAdapter.cpp" line="94"/>
@@ -181,28 +195,28 @@
 <context>
     <name>QObject</name>
     <message>
+        <location filename="src/main.cpp" line="36"/>
         <location filename="src/main.cpp" line="37"/>
-        <location filename="src/main.cpp" line="38"/>
         <source>Partition Manager</source>
         <translation>Administrador de particiones</translation>
     </message>
     <message>
-        <location filename="src/main.cpp" line="43"/>
+        <location filename="src/main.cpp" line="42"/>
         <source>Superuser privileges required.</source>
         <translation>Se necesitan permisos de superusuario.</translation>
     </message>
     <message>
-        <location filename="src/main.cpp" line="44"/>
+        <location filename="src/main.cpp" line="43"/>
         <source>This program needs superuser privileges to access storage devices. Please run this program again as root.</source>
         <translation>Este programa necesita permisos de superusuario para acceder a los dispositivos de almacenamiento. Por favor ejecute este programa nuevamente como root.</translation>
     </message>
     <message>
-        <location filename="src/main.cpp" line="51"/>
+        <location filename="src/main.cpp" line="50"/>
         <source>Another instance is open</source>
         <translation>Otra instancia está abierta</translation>
     </message>
     <message>
-        <location filename="src/main.cpp" line="52"/>
+        <location filename="src/main.cpp" line="51"/>
         <source>There is another instance of this program running. Running multiple instances is not supported.</source>
         <translation>Hay otra instancia de este programa ejecutándose. La ejecución múltiples instancias no está permitida.</translation>
     </message>
